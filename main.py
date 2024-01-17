e0593ed40d5a9bdc449bf156dd70b6fbd3d4411d
Sebastian Balzer <sebastian.balzer@gmx.ch>
2024-01-17 09:50:10 +0100

    working as intended

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-import os
 import hydra
 
-import pandas as pd
 from loguru import logger
-from omegaconf import DictConfig, OmegaConf
-from pathlib import Path
+from omegaconf import DictConfig
 
 from utils.cleaner import Cleaner
 
 
 @hydra.main(version_base=None, config_path='.', config_name='config')
 def main(config: DictConfig) -> None:
-    Cleaner(config)()
+    new_data = Cleaner(config)()
+    new_data.to_csv(config.input_dir + '_cleaned.csv', index=False)
 
 if __name__ == '__main__':
     main()
--- utils/cleaner.py
+++ utils/cleaner.py
@@ -1,7 +1,6 @@
 import glob
 
 import pandas as pd
-import numpy as np
 from loguru import logger
 
 
@@ -9,36 +8,36 @@ class Cleaner:
     def __init__(self, config) -> None:
         self.input_dir = config.input_dir
         self.rename_dict = config.rename_dict
+        self.min_duration = config.min_duration
 
     def __call__(self) -> pd.DataFrame:
         files = glob.glob(self.input_dir + '/*.csv')
-
         all_grand_rounds = []
-        for file in files[5:6] + files[8:9]:
-            logger.debug(file)
+
+        for file in files:
             data = pd.read_csv(file)
-            while data.columns[0] != 'Name (Originalname)':  # find first row to read
+            while data.columns[0] != 'Name (Originalname)':  # find first row to read (differs between files)
                 new_header = data.iloc[0]
-                data = data[1:]
+                data = data[1:]  # remove current header row
                 data.columns = new_header
             data = data.rename(columns=self.rename_dict)
-            data = data[['name', 'mail', 'duration']]
-            data['duration'] = data['duration'].astype(int)
-            # TODO: remove nadia mail from host
-            
-            # TODO: keep first mail per name, but group by name only
-            data = data.groupby(['name', 'mail']).sum().reset_index()
-            logger.debug(data.head())
-            data = data.loc[data['duration'] > 20]
-            data['count'] = 1
+            data = data[['name', 'mail', 'duration']]  # keep only relevant columns
+
+            # use loc notation to avoid SettingWithCopyWarning
+            data.loc[:, 'duration'] = data['duration'].astype(int)
+            data.loc[:, 'name'] = data['name'].apply(lambda text: text.partition('(')[0].strip())  # clean names
+
+            data = data.groupby('name', as_index=False).agg({'mail': 'first', 'duration': 'sum'})
+            data = data.loc[data['duration'] >= self.min_duration]
+            data['credits'] = 1
             all_grand_rounds.append(data)
 
         complete_data = pd.concat(all_grand_rounds)
         complete_data = complete_data.drop(columns=['duration'])
-        complete_data = complete_data.groupby(['name', 'mail']).sum().reset_index()
+        complete_data = complete_data.groupby(['name'], as_index=False).agg({'mail': 'first', 'credits': 'sum'})
 
+        # highlight duplicate mails for manual inspection
+        complete_data['mail_duplicate'] = complete_data['mail'].duplicated(keep=False)
+        complete_data.loc[complete_data['mail'].isna(), 'mail_duplicate'] = False
 
-        logger.debug(complete_data.head())
-        logger.debug(complete_data.shape)
-
-    
+        return complete_data
